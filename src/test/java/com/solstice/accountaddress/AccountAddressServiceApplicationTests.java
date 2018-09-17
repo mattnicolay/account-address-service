be8bcdda7741a6af80a302820f1d42b025f4deaf
Matt Nicolay <mnicolay@solstice.com>
2018-09-17 15:13:43 -0500

    add jenkins-test profile to application.yml

--- src/test/java/com/solstice/accountaddress/AccountAddressServiceApplicationTests.java
+++ src/test/java/com/solstice/accountaddress/AccountAddressServiceApplicationTests.java
@@ -3,14 +3,12 @@ package com.solstice.accountaddress;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class AccountAddressServiceApplicationTests {
 
-  @Test
-  public void contextLoads() {
-  }
+
 
 }
--- src/test/java/com/solstice/accountaddress/dao/AccountRepositoryUnitTests.java
+++ src/test/java/com/solstice/accountaddress/dao/AccountRepositoryUnitTests.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
@@ -25,6 +26,7 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
     DbUnitTestExecutionListener.class})
 @DatabaseSetup("classpath:test-dataset.xml")
+@ActiveProfiles("test")
 public class AccountRepositoryUnitTests {
 
   @Autowired
